5f6159519b5ff5f6e1a0d2e75213278c6f957ec2
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-04-25 04:11:52 +0000

    added the bubble sort task

---
+++ 0-bubble_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+/**
+ * swap - its swaps value of each variable
+ *
+ * @x: first variable
+ * @y: second variable
+**/
+void swap(int *x, int *y)
+{
+	int temp;
+
+	temp = *x;
+	*x = *y;
+	*y = temp;
+}
+/**
+ * bubble_sort - compares two element of a list and swaps them if not
+ * in correct position.
+ *
+ * @array: the list to be sorted
+ * @size: length of the array
+**/
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+	}
+}
